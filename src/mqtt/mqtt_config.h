78fed39fc136aa2c81f6783db34757d616c8335d
Oleksii <AlexiyPokFS@gmail.com>
2026-09-21 16:49:48 +0200

    MQTT config has been changed

--- src/mqtt/mqtt_config.h
+++ src/mqtt/mqtt_config.h
@@ -4,6 +4,9 @@
 
 // ---------------------------------
 
+#define MQTT_BROKER "broker.hivemq.com"
+#define MQTT_PORT 1883
+
 #define MQTT_TOP_TOPIC "iot-course/OleksiiPok"
 
 #define TOPIC_COMMANDS MQTT_TOP_TOPIC "/commands"
