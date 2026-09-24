24ca57307451f432a4ba897d202590a8b5850617
Oleksii <AlexiyPokFS@gmail.com>
2026-09-24 11:40:38 +0200

    time err mask

--- src/system/system_state.h
+++ src/system/system_state.h
@@ -10,6 +10,7 @@
 #define SYSTEM_LDR_ERR_MASK (1U << 1) // LDR error
 #define SYSTEM_DHT_ERR_MASK (1U << 2) // DHT22 error
 
+#define SYSTEM_TIME_ERR_MASK (1U << 5) // Time synchronization error
 #define SYSTEM_MQTT_ERR_MASK (1U << 6) // MQTT error
 #define SYSTEM_WIFI_ERR_MASK (1U << 7) // Wi-Fi error
 
@@ -18,8 +19,8 @@
 #define SYSTEM_STATE_MANAGED_MASK (SYSTEM_SILENT_MASK | SYSTEM_COMMAND_MASK)
 
 #define SYSTEM_STATE_ERR_INIT_MASK                                             \
-  (SYSTEM_LDR_ERR_MASK | SYSTEM_DHT_ERR_MASK | SYSTEM_MQTT_ERR_MASK |          \
-   SYSTEM_WIFI_ERR_MASK)
+  (SYSTEM_LDR_ERR_MASK | SYSTEM_DHT_ERR_MASK | SYSTEM_TIME_ERR_MASK |          \
+   SYSTEM_MQTT_ERR_MASK | SYSTEM_WIFI_ERR_MASK)
 
 // ---------------------------------
 
